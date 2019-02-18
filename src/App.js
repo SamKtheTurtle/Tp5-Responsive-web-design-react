77fde7a93fca4af52325ed53e4a5a35a2ddeb615
unknown <sam.caddeo@edu.ece.fr>
2019-02-18 15:04:52 +0100

    ajout col et row

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import Header from './container/Header';
 import SideMenu from './container/SideMenu';
 import Artist from './component/Artist';
 import AlbumGrid from './container/AlbumGrid';
+import {Container, Row, Col} from 'reactstrap';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div>  
+        <Container>  
         <Header />
+        <Row>
+        <Col sm={0} lg={2}>
         <SideMenu />
+        </Col>
+        <Col sm={12} lg={10}>
         <main>
           <Artist />
           <AlbumGrid />
         </main>
+        </Col>
+        </Row>
+        </Container>
+        
       </div>
     );
   }
